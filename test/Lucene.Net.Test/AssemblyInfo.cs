5e46351964148703f55debf7c9f646b823b5f5d8
Pawel Pekrol <ppekrol@ravendb.net>
2017-05-26 22:02:14 +0200

    RavenDB-6780 bumped version to 3.0.8

--- test/Lucene.Net.Test/AssemblyInfo.cs
+++ test/Lucene.Net.Test/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDefaultAlias("Lucene.Net")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyInformationalVersionAttribute("3.0.7")]
+[assembly: AssemblyInformationalVersionAttribute("3.0.8")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -50,7 +50,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.7")]
+[assembly: AssemblyVersion("3.0.8")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
