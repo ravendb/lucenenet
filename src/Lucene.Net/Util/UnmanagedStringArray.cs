1cf23a13377bc27c6623c05e4dbf272ad264770d
Grisha Kotler <grisha@ayende.com>
2023-03-23 11:22:06 +0200

    RavenDB-20137 - save the first term in order to preserve the order

--- src/Lucene.Net/Util/UnmanagedStringArray.cs
+++ src/Lucene.Net/Util/UnmanagedStringArray.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Lucene.Net.Index;
 
 namespace Lucene.Net.Util
 {
@@ -213,10 +214,18 @@ namespace Lucene.Net.Util
             _index++;
         }
 
-        public void AddEmpty()
+        public void AddEmpty(TermBuffer termBuffer)
         {
             // since we are doing a binary search, we must keep the order of the terms
-            _strings[_index].Start = _index > 0 ? _strings[_index - 1].Start : null;
+
+            if (_index == 0)
+            {
+                // we must allocate the first one
+                Add(termBuffer.TextAsSpan);
+                return;
+            }
+
+            _strings[_index].Start = _strings[_index - 1].Start;
             _index++;
         }
 
