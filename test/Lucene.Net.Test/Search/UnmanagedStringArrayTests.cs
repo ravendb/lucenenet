8affca27a336e7fb5033cb6938f4490a2e366a93
Grisha Kotler <grisha@ayende.com>
2023-03-23 16:29:15 +0200

    RavenDB-20137 - fix compilation

--- test/Lucene.Net.Test/Search/UnmanagedStringArrayTests.cs
+++ test/Lucene.Net.Test/Search/UnmanagedStringArrayTests.cs
@@ -5,151 +5,152 @@ using Lucene.Net.Util;
 using NUnit.Framework;
 using static Lucene.Net.Util.UnmanagedStringArray;
 
-namespace Lucene.Net.Search;
-
-public class UnmanagedStringArrayTests
+namespace Lucene.Net.Search
 {
-    [Test]
-    public void Should_Find_All_Terms()
+    public class UnmanagedStringArrayTests
     {
-        var terms = new UnmanagedStringArray(11);
-
-        for (var letter = 'a'; letter <= 'j'; letter++)
+        [Test]
+        public void Should_Find_All_Terms()
         {
-            terms.Add(new Span<char>(letter.ToString().ToCharArray()));
-        }
+            var terms = new UnmanagedStringArray(11);
 
-        for (var i = 1; i < terms.Length; i++)
-        {
-            var position = FieldComparator.BinarySearch(terms, terms[i]);
-            Assert.AreEqual(i, position);
-        }
-
-        VerifyNonExistingTerms(terms);
-    }
-
-    [Test]
-    public void Should_Find_With_Missing_Terms()
-    {
-        var terms = new UnmanagedStringArray(11);
-
-        var count = 0;
-        for (var letter = 'a'; letter <= 'j'; letter++)
-        {
-            if (++count == 5)
-            {
-                var termBuffer = new TermBuffer();
-                termBuffer.Set(new Term("test", letter.ToString()));
-                terms.AddDeleted(termBuffer);
-            }
-            else
+            for (var letter = 'a'; letter <= 'j'; letter++)
             {
                 terms.Add(new Span<char>(letter.ToString().ToCharArray()));
             }
-        }
 
-        for (var i = 1; i < terms.Length; i++)
-        {
-            if (i == 5)
-                continue;
-
-            var position = FieldComparator.BinarySearch(terms, terms[i]);
-            // in position 5 we save the same value as in position 4
-            var expected = i == 4 ? 5 : i;
-            Assert.AreEqual(expected, position);
-        }
-
-        VerifyNonExistingTerms(terms);
-
-        terms = new UnmanagedStringArray(11);
-
-        for (var letter = 'a'; letter <= 'j'; letter++)
-        {
-            if (letter != 'j')
+            for (var i = 1; i < terms.Length; i++)
             {
-                var termBuffer = new TermBuffer();
-                termBuffer.Set(new Term("test", letter.ToString()));
-                terms.AddDeleted(termBuffer);
+                var position = FieldComparator.BinarySearch(terms, terms[i]);
+                Assert.AreEqual(i, position);
             }
-            else
-            {
-                terms.Add(new Span<char>(letter.ToString().ToCharArray()));
-            }
+
+            VerifyNonExistingTerms(terms);
         }
 
-        for (var i = 1; i < terms.Length; i++)
+        [Test]
+        public void Should_Find_With_Missing_Terms()
         {
-            if (i != 10)
-                continue;
+            var terms = new UnmanagedStringArray(11);
 
-            var position = FieldComparator.BinarySearch(terms, terms[i]);
-            Assert.AreEqual(10, position);
+            var count = 0;
+            for (var letter = 'a'; letter <= 'j'; letter++)
+            {
+                if (++count == 5)
+                {
+                    var termBuffer = new TermBuffer();
+                    termBuffer.Set(new Term("test", letter.ToString()));
+                    terms.AddDeleted(termBuffer);
+                }
+                else
+                {
+                    terms.Add(new Span<char>(letter.ToString().ToCharArray()));
+                }
+            }
+
+            for (var i = 1; i < terms.Length; i++)
+            {
+                if (i == 5)
+                    continue;
+
+                var position = FieldComparator.BinarySearch(terms, terms[i]);
+                // in position 5 we save the same value as in position 4
+                var expected = i == 4 ? 5 : i;
+                Assert.AreEqual(expected, position);
+            }
+
+            VerifyNonExistingTerms(terms);
+
+            terms = new UnmanagedStringArray(11);
+
+            for (var letter = 'a'; letter <= 'j'; letter++)
+            {
+                if (letter != 'j')
+                {
+                    var termBuffer = new TermBuffer();
+                    termBuffer.Set(new Term("test", letter.ToString()));
+                    terms.AddDeleted(termBuffer);
+                }
+                else
+                {
+                    terms.Add(new Span<char>(letter.ToString().ToCharArray()));
+                }
+            }
+
+            for (var i = 1; i < terms.Length; i++)
+            {
+                if (i != 10)
+                    continue;
+
+                var position = FieldComparator.BinarySearch(terms, terms[i]);
+                Assert.AreEqual(10, position);
+            }
+
+            VerifyNonExistingTerms(terms);
+
+            terms = new UnmanagedStringArray(11);
+
+            for (var letter = 'a'; letter <= 'j'; letter++)
+            {
+                if (letter != 'a' && letter != 'j')
+                {
+                    var termBuffer = new TermBuffer();
+                    termBuffer.Set(new Term("test", letter.ToString()));
+                    terms.AddDeleted(termBuffer);
+                }
+                else
+                {
+                    terms.Add(new Span<char>(letter.ToString().ToCharArray()));
+                }
+            }
+
+            for (var i = 1; i < terms.Length; i++)
+            {
+                var position = FieldComparator.BinarySearch(terms, terms[i]);
+
+                switch (i)
+                {
+                    case 1:
+                        Assert.AreEqual(5, position);
+                        break;
+                    case 10:
+                        Assert.AreEqual(i, position);
+                        break;
+                }
+            }
+
+            VerifyNonExistingTerms(terms);
         }
 
-        VerifyNonExistingTerms(terms);
-
-        terms = new UnmanagedStringArray(11);
-
-        for (var letter = 'a'; letter <= 'j'; letter++)
+        private static unsafe void VerifyNonExistingTerms(UnmanagedStringArray terms)
         {
-            if (letter != 'a' && letter != 'j')
+            using (var segment = new Segment(10))
             {
-                var termBuffer = new TermBuffer();
-                termBuffer.Set(new Term("test", letter.ToString()));
-                terms.AddDeleted(termBuffer);
+                const string smallerThan = "A";
+                const string biggerThan = "z";
+
+                var size = (ushort)Encoding.UTF8.GetByteCount(smallerThan);
+                segment.Add(size, out var position);
+                Encoding.UTF8.GetBytes(smallerThan, new Span<byte>(position + sizeof(ushort), size));
+                var smallerUnmanagedString = new UnmanagedString
+                {
+                    Start = position
+                };
+
+                var result = FieldComparator.BinarySearch(terms, smallerUnmanagedString);
+                Assert.AreEqual(-2, result);
+
+                size = (ushort)Encoding.UTF8.GetByteCount(biggerThan);
+                segment.Add(size, out position);
+                Encoding.UTF8.GetBytes(biggerThan, new Span<byte>(position + sizeof(ushort), size));
+                var biggerUnmanagedString = new UnmanagedString
+                {
+                    Start = position
+                };
+
+                result = FieldComparator.BinarySearch(terms, biggerUnmanagedString);
+                Assert.AreEqual(-12, result);
             }
-            else
-            {
-                terms.Add(new Span<char>(letter.ToString().ToCharArray()));
-            }
-        }
-
-        for (var i = 1; i < terms.Length; i++)
-        {
-            var position = FieldComparator.BinarySearch(terms, terms[i]);
-
-            switch (i)
-            {
-                case 1:
-                    Assert.AreEqual(5, position);
-                    break;
-                case 10:
-                    Assert.AreEqual(i, position);
-                    break;
-            }
-        }
-
-        VerifyNonExistingTerms(terms);
-    }
-
-    private static unsafe void VerifyNonExistingTerms(UnmanagedStringArray terms)
-    {
-        using (var segment = new Segment(10))
-        {
-            const string smallerThan = "A";
-            const string biggerThan = "z";
-
-            var size = (ushort) Encoding.UTF8.GetByteCount(smallerThan);
-            segment.Add(size, out var position);
-            Encoding.UTF8.GetBytes(smallerThan, new Span<byte>(position + sizeof(ushort), size));
-            var smallerUnmanagedString = new UnmanagedString
-            {
-                Start = position
-            };
-
-            var result = FieldComparator.BinarySearch(terms, smallerUnmanagedString);
-            Assert.AreEqual(-2, result);
-
-            size = (ushort) Encoding.UTF8.GetByteCount(biggerThan);
-            segment.Add(size, out position);
-            Encoding.UTF8.GetBytes(biggerThan, new Span<byte>(position + sizeof(ushort), size));
-            var biggerUnmanagedString = new UnmanagedString
-            {
-                Start = position
-            };
-
-            result = FieldComparator.BinarySearch(terms, biggerUnmanagedString);
-            Assert.AreEqual(-12, result);
         }
     }
 }
