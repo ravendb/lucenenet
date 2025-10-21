a061b16acee62fbfcf9bbfd0ddb73a496d07754f
Grisha Kotler <grisha@ravendb.net>
2025-10-21 11:47:22 +0300

    RavenDB-24810 - allow to disable the usage of the UnmanagedArray and use only the ManagedArray

--- src/Lucene.Net/Util/HybridArray.cs
+++ src/Lucene.Net/Util/HybridArray.cs
@@ -7,12 +7,14 @@ public static class HybridArray
     // simple threshold: 64KB (guarantees ArrayPool won't exceed 85KB LOH limit)
     private const int LOH_THRESHOLD = 64 * 1024;
 
+    public static bool UseOnlyManagedArray = false;
+
     public static unsafe IArray<T> Create<T>(int length, UnmanagedStringArray.Type type, bool clear = false) where T : unmanaged
     {
         if (length < 0)
             throw new ArgumentOutOfRangeException(nameof(length));
 
-        if (length * sizeof(T) > LOH_THRESHOLD)
+        if (UseOnlyManagedArray == false && length * sizeof(T) > LOH_THRESHOLD)
         {
             return new UnmanagedArray<T>(length, type, clear);
         }
